d60fa7a888c4062b06cb21f4e1270f16fa6504c9
tw31120071857 <93863760+tw31120071857@users.noreply.github.com>
2022-09-13 22:04:33 +1000

    Fixing bugs with step tracker

--- main.ts
+++ main.ts
@@ -1,37 +1,64 @@
 input.onButtonPressed(Button.A, function () {
-    showing_the_time = 1
-    basic.clearScreen()
-    basic.showString("" + (hour))
-    basic.pause(100)
-    basic.showString("" + (minute))
-    showing_the_time = 0
+    if (page == 1) {
+        showing_the_time = 1
+        basic.clearScreen()
+        basic.showString("" + (hour))
+        basic.pause(100)
+        basic.showString("" + (minute))
+        showing_the_time = 0
+    }
+    if (page == 2) {
+        Showing_Step_Count = 1
+        basic.showString("" + (Step_Tracker))
+        Showing_Step_Count = 0
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    page += 1
 })
 input.onButtonPressed(Button.B, function () {
-    showing_the_time = 2
-    basic.clearScreen()
-    basic.showString("" + (date_day))
-    basic.showLeds(`
-        . . . . #
-        . . . # .
-        . . # . .
-        . # . . .
-        # . . . .
-        `)
-    basic.showString("" + (date_month))
-    basic.showLeds(`
-        . . . . #
-        . . . # .
-        . . # . .
-        . # . . .
-        # . . . .
-        `)
-    basic.clearScreen()
-    basic.pause(500)
-    basic.showString("" + (date_year))
-    showing_the_time = 0
+    if (page == 1) {
+        showing_the_time = 2
+        basic.clearScreen()
+        basic.showString("" + (date_day))
+        basic.showLeds(`
+            . . . . #
+            . . . # .
+            . . # . .
+            . # . . .
+            # . . . .
+            `)
+        basic.showString("" + (date_month))
+        basic.showLeds(`
+            . . . . #
+            . . . # .
+            . . # . .
+            . # . . .
+            # . . . .
+            `)
+        basic.clearScreen()
+        basic.pause(500)
+        basic.showString("" + (date_year))
+        showing_the_time = 0
+    }
+    if (page == 2) {
+        Step_Tracker = 0
+        basic.showString("" + (Step_Tracker))
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    if (page == 2) {
+        Step_Tracker += 1
+        if (Showing_Step_Count == 1) {
+            basic.showString("" + (Step_Tracker))
+        }
+    }
 })
 let minute = 0
 let showing_the_time = 0
+let Showing_Step_Count = 0
+let Step_Tracker = 0
+let page = 0
 let date_year = 0
 let date_month = 0
 let date_day = 0
@@ -40,6 +67,13 @@ hour = 610
 date_day = 13
 date_month = 9
 date_year = 2022
+// 1 = Clock
+// 2 = Step tracker.
+page = 1
+Step_Tracker = 0
+// 0 = Animation
+// 1= Show step count.
+Showing_Step_Count = 0
 basic.forever(function () {
     if (hour == 25) {
         date_day += 1
@@ -146,30 +180,77 @@ basic.forever(function () {
     }
 })
 basic.forever(function () {
-    if (showing_the_time == 0) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `)
+    if (page == 2) {
+        if (Showing_Step_Count == 0) {
+            basic.showLeds(`
+                . . # . .
+                # # # # #
+                . . # . .
+                . # . # .
+                # . . . #
+                `)
+            if (Showing_Step_Count == 0) {
+                basic.showLeds(`
+                    . # . . .
+                    # # # # .
+                    . # . . .
+                    # # # . .
+                    . . . # .
+                    `)
+                if (Showing_Step_Count == 0) {
+                    basic.showLeds(`
+                        . . # . .
+                        # # # # #
+                        . . # . .
+                        . # # # .
+                        # . . . #
+                        `)
+                    if (Showing_Step_Count == 0) {
+                        basic.showLeds(`
+                            . . . # .
+                            . # # # #
+                            . . . # .
+                            . . # # #
+                            . # . . .
+                            `)
+                    }
+                }
+            }
+        }
+    }
+})
+basic.forever(function () {
+    if (page == 3) {
+        page = 1
+    }
+})
+basic.forever(function () {
+    if (page == 1) {
         if (showing_the_time == 0) {
             basic.showLeds(`
-                # # # # #
-                # # # # #
-                # # # # #
-                # # # # #
-                # # # # #
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
                 `)
             if (showing_the_time == 0) {
                 basic.showLeds(`
-                    . . . . .
-                    . . . . .
-                    . . . . .
-                    . . . . .
-                    . . . . .
+                    # # # # #
+                    # # # # #
+                    # # # # #
+                    # # # # #
+                    # # # # #
                     `)
+                if (showing_the_time == 0) {
+                    basic.showLeds(`
+                        . . . . .
+                        . . . . .
+                        . . . . .
+                        . . . . .
+                        . . . . .
+                        `)
+                }
             }
         }
     }
